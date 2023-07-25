abc51e7f4c3168333357729635c2ecc9d5cffb36
jgimfig <jgimenezfigueroa@gmail.com>
2023-07-25 12:55:00 +0200

    quitTrama added

--- authoritation.py
+++ authoritation.py
@@ -4,10 +4,10 @@ import os
 
 API_HASH =  "e7e863f305866da6e453c1028d11cc37"
 API_ID = 25564784
-token = "6073032396:AAF-VXsRsf1Ze0ujUwL05ScX5Ucy_-NiXl8"
+token_bot = "6073032396:AAF-VXsRsf1Ze0ujUwL05ScX5Ucy_-NiXl8"
 
 app = Client("trama bot",
         api_hash=os.environ.get('API_HASH', API_HASH),
-        api_id=os.environ.get('API_ID', API_ID), bot_token=token)
+        api_id=os.environ.get('API_ID', API_ID), bot_token=token_bot)
 
 app.run()
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ async def help(_, message):
     text="Los comandos disponibles son los siguientes:\n"\
     "/help Muestra la ayuda\n"\
     "/listaTramas Muestra cada Persona con su número de tramas\n"\
-    "/addTrama + @usuario: Añade una en 1 la cuenta de las tramas del usuario mencionado"    
+    "/addTrama + @usuario: Añade en 1 la cuenta de las tramas del usuario mencionado"\
+    "/quitTrama + @usuario: Elimina en 1 la cuenta de las tramas del usuario mencionado"    
     await app.send_message(message.chat.id,text)
 
 @app.on_message(filters.command("start"))
@@ -52,7 +53,38 @@ async def addTrama(_, message):
             await app.send_message(chat_id,"Usuario no encontrado prueba de nuevo")
         else:
             pjson["People"]=people
-            # Writing to sample.json
+            # Writing to tramas.json
+        with open("tramas.json", "w") as outfile:
+            outfile.write(json.dumps(pjson,indent=2))
+        await listaTrama(_,message)
+    #If the format in telegram is not correct raise this exception
+    except IndexError:
+        await app.send_message(chat_id,"Formato incorrecto prueba de nuevo")
+        name="none"
+
+@app.on_message(filters.command("quitTrama"))
+async def addTrama(_, message):
+    chat_id=message.chat.id
+    text=message.text
+    try:
+        name=text.split('@')[1] #Name of the person 
+        with open('tramas.json', 'r') as openfile: 
+        # Reading from json file
+            pjson = json.loads(openfile.read())
+        people=pjson["People"]
+        enc=0
+        i=0
+        while(enc==0 and i< len(people)):
+            if(people[i]["Nombre"]==name):
+                enc=1
+                people[i]["Tramas"]-=1
+            else:
+                i+=1
+        if (enc==0):
+            await app.send_message(chat_id,"Usuario no encontrado prueba de nuevo")
+        else:
+            pjson["People"]=people
+            # Writing to tramas.json
         with open("tramas.json", "w") as outfile:
             outfile.write(json.dumps(pjson,indent=2))
         await listaTrama(_,message)
